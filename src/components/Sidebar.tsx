8ab1c82eef7a73431f943190d0918aad4b50e132
Honey Patel <honeydpatel2523@gmail.com>
2024-06-13 17:24:52 +0530

    feat: make responsive

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,56 +1,48 @@
 import { NavLink } from 'react-router-dom';
 import { routes } from '../routes';
-import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-} from '@/components/ui/accordion';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
 
 const Sidebar = () => {
-    return (
-        <div className="flex flex-col h-screen p-6 overflow-auto bg-gray-800 text-white">
-            <div className="text-2xl font-bold mb-6">Charts</div>
-            {routes.map((route) => {
-                if (route.children && route.name !== "Home") {
+  return (
+    <div className="flex flex-col h-screen p-6 overflow-auto bg-gray-800 text-white">
+      <div className="text-2xl font-bold mb-6">Charts</div>
+      {routes.map((route) => {
+        if (route.children && route.name !== 'Home') {
+          return (
+            <Accordion key={route.name} type="single" collapsible>
+              <AccordionItem value={route.name}>
+                <AccordionTrigger>{route.name}</AccordionTrigger>
+                <AccordionContent>
+                  {route.children.map((childRoute) => {
                     return (
-                        <Accordion key={route.name} type="single" collapsible>
-                            <AccordionItem value={route.name}>
-                                <AccordionTrigger>{route.name}</AccordionTrigger>
-                                <AccordionContent>
-                                    {route.children.map((childRoute) => {
-                                        return (
-                                            <NavLink
-                                                key={childRoute.name}
-                                                to={childRoute.path}
-                                                className={({ isActive }) =>
-                                                    `p-2 hover:text-gray-400 block ${isActive && 'text-gray-400'
-                                                    }`
-                                                }
-                                            >
-                                                {childRoute.name}
-                                            </NavLink>
-                                        );
-                                    })}
-                                </AccordionContent>
-                            </AccordionItem>
-                        </Accordion>
-                    );
-                }
-                return (
-                    <NavLink
-                        key={route.name}
-                        to={route.path}
+                      <NavLink
+                        key={childRoute.name}
+                        to={childRoute.path}
                         className={({ isActive }) =>
-                            `p-2 hover:text-gray-400 ${isActive && 'text-gray-400'}`
+                          `p-2 hover:text-gray-400 block text-left ${isActive && 'text-gray-400'}`
                         }
-                    >
-                        {route.name}
-                    </NavLink>
-                );
-            })}
-        </div>
-    );
+                      >
+                        {childRoute.name}
+                      </NavLink>
+                    );
+                  })}
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          );
+        }
+        return (
+          <NavLink
+            key={route.name}
+            to={route.path}
+            className={({ isActive }) => `p-2 hover:text-gray-400 ${isActive && 'text-gray-400'}`}
+          >
+            {route.name}
+          </NavLink>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Sidebar;
--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,17 +1,40 @@
-import { Outlet } from "react-router-dom"
-import Sidebar from "../components/Sidebar"
+import { Outlet } from 'react-router-dom';
+import Sidebar from '../components/Sidebar';
+import { Suspense } from 'react';
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTrigger } from '@/components/ui/sheet';
+import { MenuIcon } from 'lucide-react';
 
 const RootLayout = () => {
-    return (
-        <div className="flex max-h-screen">
-            <div className="w-1/5">
-                <Sidebar />
-            </div>
-            <div className="w-4/5 h-screen overflow-auto flex flex-col">
-                <Outlet />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="flex md:flex-row flex-col max-h-screen">
+      {/* Sidebar for larger screens */}
+      <div className="hidden md:block md:w-2/6 lg:w-1/5">
+        <Sidebar />
+      </div>
 
-export default RootLayout
+      {/* Sheet for smaller screens */}
+      <div className="md:hidden">
+        <Sheet>
+          <SheetTrigger className="p-4">
+            <MenuIcon />
+          </SheetTrigger>
+          <SheetContent side={'left'} className="p-0">
+            <SheetHeader>
+              <SheetDescription>
+                <Sidebar />
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
+
+      <div className="lg:w-4/5 md:w-4/6 w-full h-screen overflow-auto flex flex-col">
+        <Suspense fallback={'loading'}>
+          <Outlet />
+        </Suspense>
+      </div>
+    </div>
+  );
+};
+
+export default RootLayout;
